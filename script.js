57f5bfa4946a2b24d2a8c91e16de2569f0a7e171
jemmy <flashyjotebele@gmail.com>
2022-10-09 11:03:45 -1100

    initial commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const bar3 = document.querySelector("#bar-3");
 const menuBar = document.querySelector("#menu-bar");
 
 hamburger.addEventListener("click", () => {
-  menuBar.classList.toggle("slide-in");
+  menuBar.classList.toggle("hide-item");
   bar1.classList.toggle("bar-1");
   bar2.classList.toggle("hide-item");
   bar3.classList.toggle("bar-3");
